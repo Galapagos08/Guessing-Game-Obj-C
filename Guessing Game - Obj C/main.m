792e44c6234129b6dfca1fdf75f565e9ca5deca5
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-15 15:59:06 -0400

    Initial Commit

---
+++ Guessing Game - Obj C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Guessing Game - Obj C
+//
+//  Created by Dan Esrey on 2016/15/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
